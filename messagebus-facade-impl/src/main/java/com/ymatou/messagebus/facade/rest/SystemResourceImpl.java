5f5dbc42c5cae46be8ef418042ec7c7698621884
王旭东 <wangxudong@ymatou.com>
2016-08-26 10:32:52 +0800

    add queue capacity

--- messagebus-facade-impl/src/main/java/com/ymatou/messagebus/facade/rest/SystemResourceImpl.java
+++ messagebus-facade-impl/src/main/java/com/ymatou/messagebus/facade/rest/SystemResourceImpl.java
@@ -28,7 +28,7 @@ public class SystemResourceImpl implements SystemResource {
     @Path("/version")
     @Override
     public String version() {
-        return "2016-08-24.1";
+        return "2016-08-26.1";
     }
 
 
