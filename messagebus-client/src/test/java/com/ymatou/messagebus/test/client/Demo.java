066aa146467e9b154589031a3559c6fa27afb93a
王旭东 <wangxudong@ymatou.com>
2016-08-29 23:45:03 +0800

    add mapdb

--- messagebus-client/src/test/java/com/ymatou/messagebus/test/client/Demo.java
+++ messagebus-client/src/test/java/com/ymatou/messagebus/test/client/Demo.java
@@ -5,12 +5,32 @@
  */
 package com.ymatou.messagebus.test.client;
 
+import java.io.File;
+import java.util.concurrent.ConcurrentMap;
+
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
+import org.mapdb.Serializer;
 
 public class Demo {
     public static void main(String[] args) {
-        DB db = DBMaker.fileDB("message.db").make();
+        File folder = new File("/usr/local/data/messagebus");
+        if (folder.exists() == false) {
+            folder.mkdirs();
+        }
+
+
+        DB db = DBMaker.fileDB("/usr/local/data/messagebus/message.db").make();
         System.out.println("make success!");
+
+        ConcurrentMap<String, String> map = db.hashMap("message", Serializer.STRING, Serializer.STRING).createOrOpen();
+        if (map.containsKey("hello")) {
+            System.out.println(map.get("hello"));
+
+        } else {
+            map.put("hello", "tony");
+        }
+
+        db.close();
     }
 }
