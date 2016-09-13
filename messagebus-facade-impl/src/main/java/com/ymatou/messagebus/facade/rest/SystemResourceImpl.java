fb75943468e38d919a36c1eba9b0a0544c1e80f3
王旭东 <wangxudong@ymatou.com>
2016-09-13 18:05:09 +0800

    change version

--- messagebus-facade-impl/src/main/java/com/ymatou/messagebus/facade/rest/SystemResourceImpl.java
+++ messagebus-facade-impl/src/main/java/com/ymatou/messagebus/facade/rest/SystemResourceImpl.java
@@ -28,7 +28,7 @@ public class SystemResourceImpl implements SystemResource {
     @Path("/version")
     @Override
     public String version() {
-        return "2016-09-13.1";
+        return "2016-09-13.2";
     }
 
 
